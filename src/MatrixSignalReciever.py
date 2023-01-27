1180dc45f1c7f69b4ee02b2949a9f5dc7551551c
JOSE <joseangeldelangel10@gmail.com>
2023-01-27 09:58:05 -0600

    matrix signal reciever implemented

---
+++ src/MatrixSignalReciever.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+"""Made by:
+	José Ángel del Ángel
+    joseangeldelangel10@gmail.com
+
+Code description:
+TODO - add description
+
+Notes:
+
+"""
+import rospy
+import Jetson.GPIO as GPIO
+from std_msgs.msg import String, Int8, Header
+
+
+class MatrixSignalReciever():
+    def __init__(self, aruco_dict = cv2.aruco.DICT_4X4_50):
+        # ________ ros atributes initialization ______
+        rospy.init_node("matrix_signal_reciever")
+        rospy.Subscriber("/matrix_signal", Int8, self.matrix_signal_callback, queue_size=1)
+        # ________ Jetson TX2 initialization ______
+        self.pin_1 = 31
+        self.pin_2 = 19
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin_1, GPIO.OUT)
+        GPIO.setup(pin_2, GPIO.OUT)
+        # ________ logic attributes initialization ______
+        self.matrix_signal_to_color_dict = {0: "matrix_off", 1: "blue", 2: "red", 3: "green"}
+        self.matrix_color = self.matrix_signal_to_color_dict[0]
+
+    def matrix_signal_callback(data):
+        self.matrix_color = self.matrix_signal_to_color_dict[data.data]        
+
+    def main(self):
+        while not rospy.is_shutdown():
+            if self.matrix_color == "matrix_off":    
+                GPIO.output(pin_1, GPIO.LOW)
+                GPIO.output(pin_2, GPIO.LOW) 
+            elif self.matrix_color == "blue":
+                GPIO.output(pin_1, GPIO.HIGH)
+                GPIO.output(pin_2, GPIO.LOW)
+            elif self.matrix_color == "red":
+                GPIO.output(pin_1, GPIO.LOW)
+                GPIO.output(pin_2, GPIO.HIGH)
+            elif self.matrix_color == "green":
+                GPIO.output(pin_1, GPIO.HIGH)
+                GPIO.output(pin_2, GPIO.HIGH)
+        GPIO.output(pin_1, GPIO.LOW)
+        GPIO.output(pin_2, GPIO.LOW)
+        GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    matrix_signal_reciever = MatrixSignalReciever()
+    matrix_signal_reciever.main()
